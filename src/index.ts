7cee8da66f1bb7c1db92096e11e4af4798788f8e
antoniovalvo <vicenteolivavaldes@gmail.com>
2026-03-10 11:09:49 -0300

    s

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,11 @@
 import { Elysia } from "elysia";
+import { SQL }    from "bun";
+import { drizzle } from "drizzle-orm/bun-sql"
+import * as schema from "./db/schema"
+import 'dotenv/config';
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
-
-console.log(
-  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
-);
+const client = new SQL(process.env.DATABASE_URL!)
+const database = drizzle({
+  client,
+  schema
+})
